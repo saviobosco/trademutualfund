c67d353ac46771b7d9d1ce6df396d558debc2a87
Saviobosco <Johnboscoomebe@yahoo.com>
2018-09-24 20:24:52 +0100

    Added files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\MakePayment;
 use App\User;
 use App\Observers\UserObserver;
 use App\Observers\MakePaymentObserver;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,6 +18,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         User::observe(UserObserver::class);
         MakePayment::observe(MakePaymentObserver::class);
     }
